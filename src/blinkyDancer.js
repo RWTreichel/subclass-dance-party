6a0272aabcb2948d68b690dddb75d179e203f45b
Richard Treichel <rwtreichel@gmail.com>
2015-05-18 20:28:02 -0400

    Complete Dancer class. Implement BlinkyDancer pseudoclassically and add a step function.

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,19 +1,20 @@
-var makeBlinkyDancer = function(top, left, timeBetweenSteps){
-  var blinkyDancer = makeDancer(top, left, timeBetweenSteps);
-
+var BlinkyDancer = function(top, left, timeBetweenSteps){
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
   // so we must keep a copy of the old version of this function
+  Dancer.apply(this, arguments);
 
-  var oldStep = blinkyDancer.step;
+  var oldStep = this.step;
 
-  blinkyDancer.step = function(){
+  this.step = function(){
+    //todo: understand this again
     // call the old version of step at the beginning of any call to this new version of step
-    oldStep();
+    oldStep.call(this);
     // toggle() is a jQuery method to show/hide the <span> tag.
     // See http://api.jquery.com/category/effects/ for this and
     // other effects you can use on a jQuery-wrapped html tag.
-    blinkyDancer.$node.toggle();
+    this.$node.toggle();
   };
-
-  return blinkyDancer;
 };
+
+BlinkyDancer.prototype = Object.create(Dancer.prototype);
+BlinkyDancer.prototype.constructor = BlinkyDancer;
